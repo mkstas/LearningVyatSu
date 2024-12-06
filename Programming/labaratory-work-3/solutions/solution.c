41ebd234ffd974db0efd7da2d6736f029a5c1063
mkstas <makarov.stn@gmail.com>
2024-12-06 13:08:16 +0300

    push

--- Programming/labaratory-work-3/solutions/solution.c
+++ Programming/labaratory-work-3/solutions/solution.c
@@ -4,44 +4,9 @@
 
 void print_menu() {
     system("cls");
-    printf("1. Ввод верхнего предела\n");
-    printf("2. Ввод нижнего предела\n");
-    printf("3. Ввод шага интегрирования\n");
-    printf("4. Расчет интеграла\n");
-    printf("5. Расчет погрешности\n");
-    printf("> ");
 }
 
 int main() {
-    setlocale(LC_ALL, "");
-
     print_menu();
-
-    int point;
-
-    scanf("%d", &point);
-
-    switch (point)
-    {
-        case 1:
-            printf("1");
-            break;
-        case 2:
-            printf("2");
-            break;
-        case 3:
-            printf("3");
-            break;
-        case 4:
-            printf("4");
-            break;
-        case 5:
-            printf("5");
-            break;
-        default:
-            print_menu();
-            break;
-    }
-
     return 0;
 }
