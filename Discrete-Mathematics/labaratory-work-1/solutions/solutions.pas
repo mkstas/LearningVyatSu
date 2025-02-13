b5c0bb3131080cded69a71160eafb897967379c9
mkstas <makarov.stn@gmail.com>
2025-02-13 03:05:49 +0300

    push

--- Discrete-Mathematics/labaratory-work-1/solutions/solutions.pas
+++ Discrete-Mathematics/labaratory-work-1/solutions/solutions.pas
@@ -1,125 +1,139 @@
 ﻿program solution;
 
-uses Crt;
-
-procedure print_menu();
-begin
-  clrscr();
-  writeln('1. Ввод мощности множеств');
-  writeln('2. Ввод множества рациональных чисел');
-  writeln('3. Ввод множества действительных чисел');
-  writeln('4. Ввод множества натуральных чисел');
-  writeln('5. Ввод множества русских букв');
-  writeln('6. Ввод множества латинских букв');
-  writeln('7. Выполнить операции с множествами');
-  writeln('0. Выход');
-end;
-
 var
-  A, B, C, X: set of byte;
-  D, E, Y, K: set of string;
-  variant, p, n, i: byte;
-  s: string;
+  A, B, C, D, E, X, Y, K: set of string;
+  p, i: byte;
+  number: real;
+  symbol: char;
+  bf: string;
 
 begin
-  print_menu();
+  repeat
+    write('Введите мощность множества (не более 10): ');
+    readln(p);
+  until (p >= 1) and (p <= 10);
   
-  while true do
+  write('Введите множество (A) действительных чисел: ');
+  
+  i := 0;
+  
+  while i < p do
   begin
-    write('> ');
-    readln(variant);
-    case variant of
-      1:
-        begin
-          write('Мощность массива: ');
-          readln(p);
-        end;
-      2:
-        begin
-          if p = 0 then
-            writeln('Не введена мощность массива')
-          else
-          begin
-            write('Множество рациональных чисел: ');
-            for i := 1 to p do
-            begin
-              read(n);
-              include(A, n);
-            end;
-          end;
-        end;
-      3:
-        begin
-          if p = 0 then
-            writeln('Не введена мощность массива')
-          else
-          begin
-            write('Множество действительных чисел: ');
-            for i := 1 to p do
-            begin
-              read(n);
-              include(B, n);
-            end;
-          end;
-        end;
-      4:
-        begin
-          if p = 0 then
-            writeln('Не введена мощность массива')
-          else
-          begin
-            write('Множество натуральных чисел: ');
-            for i := 1 to p do
-            begin
-              read(n);
-              include(C, n);
-            end;
-          end;
-        end;
-      5:
-        begin
-          if p = 0 then
-            writeln('Не введена мощность массива')
-          else
-          begin
-            write('Множество русских букв: ');
-            for i := 1 to p do
-            begin
-              read(s);
-              include(E, s);
-            end;
-          end;
-        end;
-      6:
-        begin
-          if p = 0 then
-            writeln('Не введена мощность массива')
-          else
-          begin
-            write('Множество латинских букв: ');
-            for i := 1 to p do
-            begin
-              read(s);
-              include(D, s);
-            end;
-          end;
-        end;
-      7:
-        begin
-          X := A * B * C;
-          Y := (D + E) - (D * E);
-
-          writeln(X);
-          writeln(Y);
-        end;
-      0:
-        begin
-          exit;
-        end
+    read(number);
+    bf := FloatToStr(number);
+    
+    if bf in B then
+    begin
+      writeln('Число ', bf, ' уже входит во множетсво');
+      write('Введите множество действительных чисел: ');
+      continue;
+    end
     else
-      begin
-        writeln('Неверный пункт меню');
-      end;
+    begin
+      include(A, bf);
+      i := i + 1;
     end;
   end;
+  
+  write('Введите множество (B) рациональных чисел: ');
+  
+  i := 0;
+  
+  while i < p do
+  begin
+    read(number);
+    bf := FloatToStr(number);
+    
+    if bf in B then
+    begin
+      writeln('Число ', bf, ' уже входит во множетсво');
+      write('Введите множество рациональных чисел: ');
+      continue;
+    end
+    else
+    begin
+      include(B, bf);
+      i := i + 1;
+    end;
+  end;
+  
+  write('Введите множество (C) натуральных чисел: ');
+  
+  i := 0;
+  
+  while i < p do
+  begin
+    read(number);
+    bf := FloatToStr(int(number));
+    
+    if bf in C then
+    begin
+      writeln('Число ', bf, ' уже входит во множетсво');
+      write('Введите корректное натуральное число: ');
+      continue;
+    end
+    else
+    begin
+      include(C, bf);
+      i := i + 1;
+    end;
+  end;
+  
+  write('Введите множество (D) латинских букв: ');
+  
+  i := 0;
+  
+  while i < p do
+  begin
+    read(symbol);
+    bf := symbol;
+    
+    if bf in D then
+    begin
+      writeln('Буква ', bf, ' уже входит во множетсво');
+      write('Введите корректную латинскую букву: ');
+      continue;
+    end
+    else
+    begin
+      include(D, bf);
+      i := i + 1;
+    end;
+  end;
+  
+  write('Введите множество (E) русских букв: ');
+  
+  i := 0;
+  
+  while i < p do
+  begin
+    read(symbol);
+    bf := symbol;
+    
+    if bf in E then
+    begin
+      writeln('Буква ', bf, ' уже входит во множетсво');
+      write('Введите корректную русскую букву: ');
+      continue;
+    end
+    else
+    begin
+      include(E, bf);
+      i := i + 1;
+    end;
+  end;
+  
+  X := A * B * C;
+  Y := (D + E) - (D * E);
+  K := X + Y;
+  
+  writeln('A = ', A);
+  writeln('B = ', B);
+  writeln('C = ', C);
+  writeln('D = ', D);
+  writeln('E = ', E);
+  writeln('X = A * B * C = ', X);
+  writeln('Y = (D + E) - (D * E) = ', Y);
+  writeln('K = X + Y = ', K);
+  writeln('Итоговая мощность множества: ', K.count);
 end.
