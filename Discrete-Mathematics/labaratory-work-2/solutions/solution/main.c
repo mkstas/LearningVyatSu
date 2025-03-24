90a5bbd6eec8d9083174283f6db19a3b91870066
mkstas <makarov.stn@gmail.com>
2025-03-24 19:34:18 +0300

    24.03.25

--- Discrete-Mathematics/labaratory-work-2/solutions/solution/main.c
+++ Discrete-Mathematics/labaratory-work-2/solutions/solution/main.c
@@ -3,91 +3,48 @@
 #include <locale.h>
 #include <time.h>
 
-void printTableHead(int n)
-{
-	printf("Матрица смежности\n    ");
-	for (int i = 0; i < n; i++) {
-		printf("%d  ", i + 1);
-	}
-	printf("\n    ");
-
-	for (int i = 0; i < n - 1; i++) {
-		printf("----");
-	}
-	printf("\n");
-}
-
-void generateAndPrintMatrix(int n, int** matrix)
-{
-	for (int i = 0; i < n; i++) {
-		printf(" %d |", i + 1);
-		for (int j = 0; j < n; j++) {
-			matrix[i][j] = rand() % 2;
-			printf(" %d ", matrix[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-void findDegree(int n, int** matrix, int* degrees)
-{
-	for (int i = 0; i < n; i++) {
-		degrees[i] = 0;
-		for (int j = 0; j < n; j++) {
-			degrees[i] += matrix[i][j];
-		}
-	}
-}
-
-int findVertexWithMaxDegree(int n, int* degrees)
-{
-	int max = degrees[0];
-	int vertex = 0;
-
-	for (int i = 0; i < n; i++) {
-		if (degrees[i] > max) {
-			max = degrees[i];
-			vertex = i + 1;
-		}
-	}
-
-	printf("Вершина с максимальной полустепенью исхода: %d\n", vertex);
-	return vertex;
-}
-
-void find_edges_from_vertex(int n, int** matrix, int vertex) {
-    printf("Рёбра, исходящие из вершины %d: ", vertex);
-    for (int i = 0; i < n; i++) {
-        if (matrix[vertex][i] == 1) {
-            printf("(%d -> %d) ", vertex, i + 1);
-        }
-    }
-    printf("\n");
-}
-
-int main()
-{
+int main() {
 	setlocale(LC_ALL, "Russian");
 	srand(time(NULL));
 
-	int n, vertex = 0;
+	int n;
 
 	do {
 		printf("Введите размерность матрицы (4 <= n <= 10): ");
 		scanf_s("%d", &n);
 	} while (n < 4 || n > 10);
 
-	int** matrix = (int*)malloc(sizeof(int) * n * n);
-	for (int i = 0; i < n; i++) {
-		matrix[i] = (int*)malloc(sizeof(int) * n);
-	}
-	int* degrees = (int*)malloc(sizeof(int) * n);
+	int matrix[10][10] = { 0 };
+	int degrees[10] = { 0 };
 
-	printTableHead(n);
-	generateAndPrintMatrix(n, matrix);
-	findDegree(n, matrix, degrees);
-	vertex = findVertexWithMaxDegree(n, degrees);
-	find_edges_from_vertex(n, matrix, vertex);
+	printf("Матрица смежности:\n");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			matrix[i][j] = rand() % 2;
+			degrees[i] += matrix[i][j];
+			printf("%d ", matrix[i][j]);
+		}
+		printf("\n");
+	}
+
+	int max = 0;
+	int vertex = 0;
+
+	for (int i = 0; i < n; i++) {
+		if (degrees[i] > max) {
+			max = degrees[i];
+			vertex = i;
+		}
+	}
+
+	printf("Вершина с максимальной полустепенью исхода: %d\n", vertex + 1);
+	printf("Множество дуг для вершины %d: {", vertex + 1);
+	for (int j = 0; j < n; j++) {
+		if (matrix[vertex][j] == 1) {
+			printf(" (%d, %d) ", vertex + 1, j + 1);
+		}
+	}
+	printf("}\n");
 
 	return 0;
 }
