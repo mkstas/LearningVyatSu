d944f97bb1dae6a96c101bd37ee50efca6dabd3d
mkstas <makarov.stn@gmail.com>
2025-04-21 06:05:58 +0300

    21.04.25

--- discrete-mathematics/labaratory-work-5/solution/solution.pas
+++ discrete-mathematics/labaratory-work-5/solution/solution.pas
@@ -4,10 +4,28 @@ uses
   SysUtils;
 
 var
-  matrix[1..4, 1..4] set of integer;
+  matrixIncid[1..4, 1..4] set of integer;
+  matrixAdjac[1..4, 1..4] set of integer;
   fileInput: text;
   fileLine: string;
+  i, j: integer;
 
 begin
+  assign(fileInput, 'input.txt');
+  reset(input.txt);
+  writeln('Матрица инцедентности', #10);
 
+  write('  ');
+  for i := 1 to 5 do
+    write(' ', i, ' ');
+  writeln();
+
+  while not Eof(fileInput) do
+  begin
+    write(i, '  ');
+    readln(fileInput, fileLine);
+    j := 1;
+  end;
+
+  close(fileInput);
 end.
