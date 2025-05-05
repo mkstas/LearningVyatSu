3b1c33a783433376188daada6eca1f917b9ec43e
mkstas <makarov.stn@gmail.com>
2025-05-05 03:04:10 +0300

    05.05.25

---
+++ programming/labaratory-work-7/solution/dragon.pas
@@ -0,0 +1,60 @@
+﻿unit Dragon;
+
+interface
+
+uses
+  GraphABC;
+
+procedure DrawFractal(x, y, sx, sy, depth: integer);
+
+implementation
+
+  var
+    angle: integer;
+
+  procedure Draw();
+  begin
+    angle := angle mod 4;
+    case angle of
+      0: LineRel(10, 0);
+      1, -3: LineRel(0, 10);
+      2, -2: LineRel(-10, 0);
+      3, -1: LineRel(0, -10);
+    end;
+  end;
+
+  procedure Fractal(depth, turn: integer);
+  begin
+    if depth < 1 then
+    begin
+      Draw();
+      exit;
+    end;
+
+    Fractal(depth - 1, 1);
+    angle := angle + turn;
+    Fractal(depth - 1, -1);
+  end;
+
+  procedure DrawFractal(x, y, sx, sy, depth: integer);
+  var
+    wrappedX, wrappedY: integer;
+    visibleWidth, visibleHeight: integer;
+  begin
+    SetCoordinateScale(sx, sy);
+
+    visibleWidth := WindowWidth div sx;
+    visibleHeight := WindowHeight div sy;
+
+    wrappedX := x mod visibleWidth;
+    wrappedY := y mod visibleHeight;
+
+    if wrappedX < 0 then wrappedX := wrappedX + visibleWidth;
+    if wrappedY < 0 then wrappedY := wrappedY + visibleHeight;
+
+    MoveTo(wrappedX, wrappedY);
+    angle := 0;
+    Fractal(depth, 1);
+  end;
+
+end.
--- programming/labaratory-work-7/solution/solution.pas
+++ programming/labaratory-work-7/solution/solution.pas
@@ -1,7 +1,62 @@
-{$codepage UTF8}
+﻿uses
+  GraphABC, Dragon;
 
-uses GraphABC, KOH;
+var
+  depth: integer = 1;
+  x: integer = 500;
+  y: integer = 150;
+  sx: integer = 1;
+  sy: integer = 1;
 
-Begin
-  KOHDraw(10, 354, 400, pi / 3, 4,1,1);
+procedure Scale(key: integer);
+begin
+  case key of
+    VK_Z:
+      begin
+        if depth <= 10 then 
+          depth += 1 
+        else
+          exit;
+      end;
+    VK_X:
+      begin
+        if depth > 1 then
+          depth -= 1
+        else
+          exit;
+      end;
+    VK_S:
+      if sx < 4 then
+      begin
+        sx := sx + 1;
+        sy := sy + 1;
+        x := 500 div sx; y := 150 div sy;
+      end;
+    VK_D:
+      if sx > 1 then
+      begin
+        sx := sx - 1;
+        sy := sy - 1;
+        x := 500 div sx; y := 150 div sy;
+      end;
+    VK_UP:
+      y := y - 10;
+    VK_DOWN:
+      y := y + 10;
+    VK_LEFT:
+      x := x - 10;
+    VK_RIGHT:
+      x := x + 10;
+  end;
+
+  Window.Clear();
+  DrawFractal(x, y, sx, sy, depth);
+end;
+
+begin
+
+  SetWindowSize(800, 600);
+  OnKeyDown := Scale;
+  DrawFractal(x, y, sx, sy, depth);
+
 end.
