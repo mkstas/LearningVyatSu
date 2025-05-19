efc28ceb177d2746118613bfbd5ff3df32cd945f
mkstas <makarov.stn@gmail.com>
2025-05-19 16:46:31 +0300

    19.05.25

--- discrete-mathematics/labaratory-work-6/solution/solution.pas
+++ discrete-mathematics/labaratory-work-6/solution/solution.pas
@@ -8,6 +8,14 @@ const
 
 type
   TMatrix = array[1..MAX_SIZE, 1..MAX_SIZE] of integer;
+  TVisited = array[1..MAX_SIZE] of boolean;
+  TPath = array[1..MAX_SIZE] of integer;
+
+var
+  adjacency: TMatrix;
+  visited: TVisited;
+  path: TPath;
+  startVertex: integer;
 
 procedure ReadMatrix(var matrix: TMatrix);
 var
@@ -56,13 +64,58 @@ begin
   end;
 end;
 
+procedure PrintPath(pathLength: integer);
 var
-  adjacency: TMatrix;
+  i: integer;
+begin
+  for i := 1 to pathLength - 1 do
+    Write(path[i], ' -> ');
+  Writeln(path[pathLength]);
+end;
+
+procedure DFS(current, target, pathLength: integer);
+var
+  i: integer;
+begin
+  visited[current] := true;
+  path[pathLength] := current;
+
+  if current <> target then
+  begin
+    for i := 1 to MAX_SIZE do
+      if (adjacency[current, i] <> 0) and not visited[i] then
+        DFS(i, target, pathLength + 1);
+  end
+  else
+    PrintPath(pathLength);
+
+  visited[current] := false;
+end;
+
+procedure FindAllPaths(startVertex: integer);
+var
+  i: integer;
+begin
+  for i := 1 to MAX_SIZE do
+    visited[i] := false;
+
+  for i := 1 to MAX_SIZE do
+    if i <> startVertex then
+      DFS(startVertex, i, 1);
+end;
 
 begin
 
   ReadMatrix(adjacency);
   Writeln(#10, 'Матрица смежности', #10);
   PrintMatrix(adjacency);
+
+  Write(#10, 'Введите начальную вершину: ');
+  Readln(startVertex);
+
+  Writeln('Список всех элементарных путей');
+  FindAllPaths(startVertex);
+
+  Readln;
 
 end.
