17eb6b4c21bf4461eb229ddd52bcb10379b0cb83
mkstas <makarov.stn@gmail.com>
2025-09-11 22:46:49 +0300

    11.09.25

--- data-managament/migration.sql
+++ data-managament/migration.sql
@@ -1,125 +1,164 @@
-DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS payments CASCADE;
-DROP TABLE IF EXISTS user_payments CASCADE;
-DROP TABLE IF EXISTS categories CASCADE;
-DROP TABLE IF EXISTS products CASCADE;
-DROP TABLE IF EXISTS ingredients CASCADE;
-DROP TABLE IF EXISTS product_ingredients CASCADE;
-DROP TABLE IF EXISTS variants CASCADE;
-DROP TABLE IF EXISTS product_variants CASCADE;
-DROP TABLE IF EXISTS carts CASCADE;
-DROP TABLE IF EXISTS cart_products CASCADE;
-DROP TABLE IF EXISTS cart_product_ingredients CASCADE;
-DROP TABLE IF EXISTS orders CASCADE;
+-- DropTable
+DROP TABLE IF EXISTS "users" CASCADE;
+DROP TABLE IF EXISTS "payments" CASCADE;
+DROP TABLE IF EXISTS "user_payments" CASCADE;
+DROP TABLE IF EXISTS "categories" CASCADE;
+DROP TABLE IF EXISTS "products" CASCADE;
+DROP TABLE IF EXISTS "ingredients" CASCADE;
+DROP TABLE IF EXISTS "product_ingredients" CASCADE;
+DROP TABLE IF EXISTS "variants" CASCADE;
+DROP TABLE IF EXISTS "product_variants" CASCADE;
+DROP TABLE IF EXISTS "carts" CASCADE;
+DROP TABLE IF EXISTS "cart_products" CASCADE;
+DROP TABLE IF EXISTS "cart_product_ingredients" CASCADE;
+DROP TABLE IF EXISTS "orders" CASCADE;
 
-CREATE TABLE users (
-    "id" BIGINT PRIMARY KEY,
+-- CreateTable
+CREATE TABLE "users" (
+    "id" BIGSERIAL PRIMARY KEY,
     "phone_number" VARCHAR(11) NOT NULL UNIQUE,
-    "fullname" VARCHAR(255) NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL
+    "username" VARCHAR(256),
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE payments (
-    "id" BIGINT PRIMARY KEY,
-    "title" VARCHAR(255) NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL
+-- CreateTable
+CREATE TABLE "payments" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "title" VARCHAR(256) NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE user_payments (
-    "id" BIGINT PRIMARY KEY,
-    "user_id" BIGINT NOT NULL REFERENCES users(id),
-    "payment_id" BIGINT NOT NULL REFERENCES payments(id),
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL
+-- CreateTable
+CREATE TABLE "user_payments" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "user_id" BIGINT NOT NULL,
+    "payment_id" BIGINT NOT NULL,
+    "card_number" VARCHAR(16),
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE,
+    FOREIGN KEY ("payment_id") REFERENCES "payments"("id") ON DELETE CASCADE
 );
 
-CREATE TABLE categories (
-    "id" BIGINT PRIMARY KEY,
-    "title" VARCHAR(255) NOT NULL UNIQUE,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "title" VARCHAR(256) NOT NULL UNIQUE,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE products (
-    "id" BIGINT PRIMARY KEY,
-    "category_id" BIGINT NOT NULL REFERENCES categories(id) ON DELETE CASCADE,
-    "title" VARCHAR(255) NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL
+-- CreateTable
+CREATE TABLE "products" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "category_id" BIGINT NOT NULL,
+    "title" VARCHAR(256) NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE RESTRICT
 );
 
-CREATE TABLE ingredients (
-    "id" BIGINT PRIMARY KEY,
-    "title" VARCHAR(255) NOT NULL UNIQUE,
-    "image_url" VARCHAR(255) NOT NULL,
+-- CreateTable
+CREATE TABLE "ingredients" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "title" VARCHAR(256) NOT NULL UNIQUE,
+    "image_url" VARCHAR(512) NOT NULL,
     "price" INT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE product_ingredients (
-    "id" BIGINT PRIMARY KEY,
-    "product_id" BIGINT NOT NULL REFERENCES products(id) ON DELETE CASCADE,
-    "ingredient_id" BIGINT NOT NULL REFERENCES ingredients(id) ON DELETE CASCADE,
-    "is_required" BOOLEAN NOT NULL DEFAULT false,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL
+-- CreateTable
+CREATE TABLE "product_ingredients" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "product_id" BIGINT NOT NULL,
+    "ingredient_id" BIGINT NOT NULL,
+    "is_required" BOOLEAN NOT NULL DEFAULT FALSE,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE RESTRICT,
+    FOREIGN KEY ("ingredient_id") REFERENCES "ingredients"("id") ON DELETE RESTRICT
 );
 
-CREATE TABLE variants (
-    "id" BIGINT PRIMARY KEY,
-    "title" VARCHAR(255) NOT NULL,
-    "value" VARCHAR(255) NOT NULL UNIQUE,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL
+-- CreateTable
+CREATE TABLE "variants" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "title" VARCHAR(256) NOT NULL,
+    "value" VARCHAR(256) NOT NULL UNIQUE,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE product_variants (
-    "id" BIGINT PRIMARY KEY,
-    "product_id" BIGINT NOT NULL REFERENCES products(id) ON DELETE CASCADE,
-    "variant_id" BIGINT NOT NULL REFERENCES variants(id) ON DELETE CASCADE,
-    "image_url" VARCHAR(255) NOT NULL,
+-- CreateTable
+CREATE TABLE "product_variants" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "product_id" BIGINT NOT NULL,
+    "variant_id" BIGINT NOT NULL,
+    "image_url" VARCHAR(512) NOT NULL,
     "price" INT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE RESTRICT,
+    FOREIGN KEY ("variant_id") REFERENCES "variants"("id") ON DELETE RESTRICT
 );
 
-CREATE TABLE cart_product_ingredients (
-    "id" BIGINT PRIMARY KEY,
-    "product_ingredient_id" BIGINT NOT NULL REFERENCES product_ingredients(id) ON DELETE CASCADE,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL
+-- CreateTable
+CREATE TABLE "carts" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "user_id" BIGINT NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE cart_products (
-    "id" BIGINT PRIMARY KEY,
-    "product_variant_id" BIGINT NOT NULL REFERENCES product_variants(id) ON DELETE CASCADE,
-    "cart_product_ingredient_id" BIGINT NOT NULL REFERENCES cart_product_ingredients(id) ON DELETE CASCADE,
+-- CreateTable
+CREATE TABLE "cart_products" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "cart_id" BIGINT NOT NULL,
+    "product_variant_id" BIGINT NOT NULL,
     "quantity" INT NOT NULL DEFAULT 0,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY ("cart_id") REFERENCES "carts"("id") ON DELETE RESTRICT,
+    FOREIGN KEY ("product_variant_id") REFERENCES "product_variants"("id") ON DELETE RESTRICT
 );
 
+-- CreateTable
+CREATE TABLE "cart_product_ingredients" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "cart_product_id" BIGINT NOT NULL,
+    "product_ingredient_id" BIGINT NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-CREATE TABLE carts (
-    "id" BIGINT PRIMARY KEY,
-    "user_id" BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    "cart_product_id" BIGINT NOT NULL REFERENCES cart_products(id) ON DELETE CASCADE,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL
+    FOREIGN KEY ("cart_product_id") REFERENCES "cart_products"("id") ON DELETE RESTRICT,
+    FOREIGN KEY ("product_ingredient_id") REFERENCES "product_ingredients"("id") ON DELETE RESTRICT
 );
 
-CREATE TABLE orders (
-    "id" BIGINT PRIMARY KEY,
-    "user_id" BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    "payment_id" BIGINT NOT NULL REFERENCES payments(id) ON DELETE CASCADE,
+create type ORDER_STATUS_ENUM as enum (
+    'pending', 'succeeded', 'canceled'
+);
+
+-- CreateTable
+CREATE TABLE "orders" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "user_id" BIGINT NOT NULL,
+    "payment_id" BIGINT NOT NULL,
     "products" JSON NOT NULL,
-    "address" VARCHAR(255) NOT NULL,
+    "status" ORDER_STATUS_ENUM DEFAULT 'pending',
+    "address" VARCHAR(256) NOT NULL,
+    "username" VARCHAR(256) NOT NULL,
     "cost" INT NOT NULL,
-    "status" VARCHAR(255) CHECK (status IN ('pending', 'succeeded', 'canceled')),
     "comment" VARCHAR(255) DEFAULT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE,
+    FOREIGN KEY ("payment_id") REFERENCES "payments"("id") ON DELETE CASCADE
 );
